d2e21769effd0a97763642de9f71a686048e0962
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-11-25 18:28:37 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function element01(){
-    let element1 = window.document.getElementById('body').style.background = 'black'
-    
+    let element1 = window.document.getElementById('element1').style.background = 'gray'
+    let element2 = window.document.getElementById('element1').style.color = 'white'
+    let element3 = window.document.getElementById('element1').style.border = '0px'
+    let res = window.document.getElementById('res')
+    res.innerHTML = '<p> A exposição a esse elemento pode gerar consequências desagradáveis. O cal, sendo virgem ou não, tem grande poder de lesão sobre o organismo humano. Se ingerido causa irritação nas mucosas da boca, garganta, esôfago, estômago e intestino. Pode causar dores abdominais e vômito.</p>'
 }
