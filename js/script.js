abf0d73d59159c11bdd6b62c822c3097109c0617
santannadkevinn <67100140+santannadkevinn@users.noreply.github.com>
2022-11-25 17:36:13 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function element1(){
-    let element1 = document.getElementById('element1')
+    let element1 = document.querySelector('option#element1')
+    let res = document.getElementById('res')
     element1.style.background('black')
+    res.innerHTML = 'merda'
 }
